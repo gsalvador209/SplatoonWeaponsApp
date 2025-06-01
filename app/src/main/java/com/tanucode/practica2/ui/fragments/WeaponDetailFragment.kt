16c3ae43d5278f68459badfb52fdb0012ae825fa
Salvador Chavez <dchavezvillanueva@gmail.com>
2025-06-01 15:13:22 -0600

    feature: implemented video display with exo player for detail view

--- app/src/main/java/com/tanucode/practica2/ui/fragments/WeaponDetailFragment.kt
+++ app/src/main/java/com/tanucode/practica2/ui/fragments/WeaponDetailFragment.kt
@@ -6,7 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
 import com.bumptech.glide.Glide
 import com.tanucode.practica2.R
 import com.tanucode.practica2.application.WeaponsRFAPP
@@ -28,6 +33,9 @@ class WeaponDetailFragment : Fragment() {
 
     //Repositorio para las funciones del API
     private lateinit var  repository: WeaponsRepository
+
+    //Para videos
+    private var exoPlayer: ExoPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,7 +63,6 @@ class WeaponDetailFragment : Fragment() {
         lifecycleScope.launch { //Cuando se crea la vista se llama al API
             try {
                 val weaponDetail = repository.getWeaponDetail(weaponId)
-
                 binding.apply {
                     tvName.text = weaponDetail.name
                     tvClass.text = weaponDetail.weaponClass
@@ -67,15 +74,22 @@ class WeaponDetailFragment : Fragment() {
                     Glide.with(requireActivity())
                         .load(weaponDetail.image)
                         .into(ivImage)
-                }
 
+                    weaponDetail.video?.let{ videoUrl->
+                        initializePlayer(videoUrl)
+                    } ?: run {
+                        exoView.visibility = View.GONE
+                        Toast.makeText(requireContext(), "Video no disponible", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
 
             }catch (e: Exception){
                 //Error de conexión
             }finally {
                 binding.pbLoading.visibility = View.GONE
             }
-            Log.d(Constants.LOGTAG, getString(R.string.id_recived, id))
+            Log.d(Constants.LOGTAG, getString(R.string.id_recived, id.toString()))
         }
     }
 
@@ -83,6 +97,31 @@ class WeaponDetailFragment : Fragment() {
         super.onDestroy()
         _binding =  null //Por el ciclo de vida tricky del fragment
         //Es necesario liberar el binding al destruirse
+    }
+
+    private fun initializePlayer(videoUrl: String){
+        exoPlayer = ExoPlayer.Builder(requireContext()).build().also { player ->
+            binding.exoView.player = player
+
+            player.repeatMode = Player.REPEAT_MODE_ONE
+
+            val mediaItem = MediaItem.fromUri(videoUrl)
+            player.setMediaItem(mediaItem)
+
+            player.addListener(object : Player.Listener {
+                override fun onPlayerError(error: PlaybackException) {
+                    binding.exoView.visibility = View.GONE
+                    Toast.makeText(
+                        requireContext(),
+                        "Error al reproducir el video",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+            player.prepare()
+
+            player.playWhenReady = true
+        }
     }
 
     companion object{
